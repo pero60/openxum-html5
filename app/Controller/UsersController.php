0fb0a45fa864b56c1ffacca51e1d8c62e0fd6c4a
Eric Ramat <eric.ramat@gmail.com>
2013-10-31 23:09:39 +0100

    Change license text in some files

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -1,8 +1,8 @@
 <?php
 /**
- *  This file is part of LibreApprenti.
+ *  This file is part of OpenXum project.
  *
- *  LibreApprenti is free software: you can redistribute it and/or modify
+ *  OpenXum is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
@@ -13,10 +13,10 @@
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with LibreApprenti.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with OpenXum.  If not, see <http://www.gnu.org/licenses/>.
  *
- * @copyright     Copyright (c) Université du Littoral - Côte d'Opale / CUEEP
- * @link          http://github.com/eramat/LibreApprenti
+ * @copyright     Copyright (c) Eric Ramat
+ * @link          http://github.com/openxum-team/openxum-html5
  * @package       app.Controller
  * @license       http://www.gnu.org/licenses/ GPLv3 License
  */
@@ -103,7 +103,7 @@ class UsersController extends AppController
         if ($this->request->is('post')) {
             if ($this->Auth->login()) {
                 if (AuthComponent::user('role') === 'student') {
-                    return $this->redirect(array('controller' => 'apprentis', 'action' => 'display'));
+                    return $this->redirect(array('controller' => 'pages', 'action' => 'display', 'home'));
                 }
                 return $this->redirect($this->Auth->redirectUrl());
             } else {
